b8346dc80c522cc5046476ee9ebcf00862ddc65c
parvez <parvezmisarwala@gmail.com>
2017-03-31 15:31:49 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM java:7
+COPY First.java .
+RUN javac First.java
+
+CMD ["java", "First"]
---
+++ First.java
@@ -0,0 +1,7 @@
+public class First{
+    public static void main(String[] args){
+        System.out.println("HEllo Parvez");
+	System.out.println("How are you doing today");
+	System.out.println("Test speed");
+    }
+}
